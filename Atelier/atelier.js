b4c4b58eca7ad2b261c31956f77b4011570a89d5
drabiot <lipilouc@gmail.com>
2026-04-18 02:40:14 +0200

    Fix numbers of Build

--- Atelier/atelier.js
+++ Atelier/atelier.js
@@ -10,7 +10,7 @@
   let filterTier 	= null;
   let activeClass   = null;
   let filterStats	= new Set();
-  const NUM_BUILDS   = 100;
+  const NUM_BUILDS   = 5;
 	let activeBuildIndex = 0;
 
   /* ══ INDEX MAPS ══
