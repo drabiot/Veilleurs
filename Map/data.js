86a2ce600a0592a422894b66d066c0110fa927a7
drabiot <lipilouc@gmail.com>
2026-04-29 02:05:34 +0200

    Calibrate floor3 map

--- Map/data.js
+++ Map/data.js
@@ -20,9 +20,15 @@ const MAP_CALIBRATION = {
     radiusGame:  2498.1,
   },
   2: {
-  centerPixel: { x: 450,  y: 450 },
-  centerGame:  { x: -1.3, y: 0.8 },
-  radiusPixel: 450,
-  radiusGame:  1072.5,
-},
+    centerPixel: { x: 450,  y: 450 },
+    centerGame:  { x: -1.3, y: 0.8 },
+    radiusPixel: 450,
+    radiusGame:  1072.5,
+  },
+  3: {
+    centerPixel: { x: 450,  y: 450 },
+    centerGame:  { x: 597, y: 771 },
+    radiusPixel: 450,
+    radiusGame:  850,
+  },
 };
